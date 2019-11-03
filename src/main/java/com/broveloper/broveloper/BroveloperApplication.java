7a6316b075d520dcae41c9008b87d5651b5552fa
tyler mcclain <email.tyler.mcclain@gmail.com>
2019-11-03 17:15:41 -0500

    Added base spring project

---
+++ src/main/java/com/broveloper/broveloper/BroveloperApplication.java
@@ -0,0 +1,13 @@
+package com.broveloper.broveloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BroveloperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BroveloperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/broveloper/broveloper/BroveloperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.broveloper.broveloper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BroveloperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
